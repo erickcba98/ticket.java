7f46468ebcc546883afb2b885f5e2f102f4f5af8
erickcba98 <erick_cba98@outlook.com>
2022-05-06 13:39:44 -0300

    Primera version

---
+++ src/producto/Producto.java
@@ -0,0 +1,48 @@
+package producto;
+
+public class Producto {
+
+    private final int idProducto;
+    private static int contadorProducto;
+    private String nombre;
+    private double precio;
+
+    private Producto() {
+        this.idProducto = ++Producto.contadorProducto;
+    }
+
+    public Producto(String nombre, double precio) {
+        this();
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{");
+        sb.append("idProducto=").append(idProducto);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+}
---
+++ src/ventas/VentasTest.java
@@ -0,0 +1,15 @@
+package ventas;
+
+import producto.*;
+
+public class VentasTest {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Camisa", 5.02);
+        Producto producto2 = new Producto("Pantalon", 12.47);
+        
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+    }
+}
